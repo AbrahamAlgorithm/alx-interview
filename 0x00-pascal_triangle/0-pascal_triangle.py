cbc6d5ffefd6172d964c1c7d03c30f15a29c5670
AbrahamAlgorithm <nightscholar0@gmail.com>
2024-05-31 07:09:29 +0200

    pascal_triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -2,13 +2,13 @@ def pascal_triangle(n):
     if n <= 0:
         return []
 
-    triangle = [[1]];
+    triangle = [[1]]
 
     for i in range(1, n):
         row = [1]
         for m in range(1, i):
             row.append(triangle[i-1][m-1] + triangle[i-1][m])
-            row.append(1)
-            triangle.append(row)
+        row.append(1)
+        triangle.append(row)
 
-        return triangle
+    return triangle
